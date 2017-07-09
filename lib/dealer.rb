b59da49cc1d2129837f428e7d0463ee95bf5fd20
Jack Bittiner <jackbittiner@hotmail.com>
2017-07-09 13:17:17 +0100

    refactored: moved reasons to fail dealing into own method

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -13,14 +13,20 @@ class Dealer
   end
 
   def deal_cards
-    fail "Deck is not shuffled!" if self.deck.shuffled != true
-    fail "Return cards and shuffle before dealing again!" if self.deck.cards.length < 52
+    able_to_deal_cards?
     7.times do
       (0..3).each do |num|
         self.players[num].hand << self.deck.cards[0]
         self.deck.cards.shift
       end
     end
+  end
+
+  private
+
+  def able_to_deal_cards?
+    fail "Deck is not shuffled!" if self.deck.shuffled != true
+    fail "Return cards and shuffle before dealing again!" if self.deck.cards.length < 52
   end
 
 end
